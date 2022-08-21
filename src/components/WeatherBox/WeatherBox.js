de12978d9836c5eb5667f55fc6253533b5c6483d
Zbigniew013 <zbigniew.szalkowski@gmail.com>
2022-08-21 12:13:43 +0200

    fetch weather api - fix

--- src/components/WeatherBox/WeatherBox.js
+++ src/components/WeatherBox/WeatherBox.js
@@ -6,11 +6,11 @@ import { useCallback } from 'react';
 const WeatherBox = (props) => {
 
   const handleCityChange = useCallback((city) => {
-    fetch(`https://api.openweathermap.org/data/2.5/weather?q=city&appid=3a46d76c45d8cf4ee0ffb61855326212&units=metric`)
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=3a46d76c45d8cf4ee0ffb61855326212&units=metric`)
       .then(response => response.json())
       .then(data => {
      console.log(data);
-   });
+    });
     console.log(city);
   }, [] );
 
